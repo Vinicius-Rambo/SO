47c1e2d28e2dec269f46bd5af2cdcc220c2a1781
Vinicius Rambo <vncs.rambo@gmail.com>
2025-10-16 09:04:17 -0300

    Update operacoesBasicas.sh

--- 01 - Bash /operacoesBasicas.sh
+++ 01 - Bash /operacoesBasicas.sh
@@ -9,61 +9,61 @@ enter(){
     read -p "Pressione ENTER para continuar .." 
 }
 
-echo "Diretorio Atual"
+echo "1. Diretorio Atual"
 pwd
 echo 
+enter
 
 #Criar um diretório de trabalho
-echo "Criando um diretório 'meu_projeto'..."
+echo "2. Criando um diretório 'meu_projeto'..."
 mkdir meu_projeto
 echo "Diretório criado"
-
-
+enter
 
 #Acessar o Diretorio meu_projeto
+echo "3. Entrando no diretório 'meu_projeto' "
 cd meu_projeto || exit
 echo "Agora estamos em ..."
 pwd
 enter
 
-#Criar três arquivos vazios
-touch arquivo1.txt, arquivo2.txt, arquivo3.txt
-
-#Listar diretorio
+#Criar três arquivos vazios e Listar diretorio
+echo "4. Criando três arquivos vazios"
+touch arquivo1.txt arquivo2.txt arquivo3.txt
 ls -l
 enter
 
-#Escrever texto em um arquivo
-
-echo "Texto de exemplo" > arquivo1.txt
-enter
-
-#Exibir o conteúdo do arquivo
-
+#Escrever texto no arquivo1 e exibir o conteúdo do arquivo
+echo "5. Escrevendo texto em arquivo1.txt..."
+echo "eu sou porque nós somos" > arquivo1.txt
 cat arquivo1.txt
+enter
 
-
-#Renomear arquivo2 para notas
+#Renomear e backup
+echo "6. Renomeando arquivo2.txt para notas.txt..."
 mv arquivo2.txt notas.txt
-enter
-
-#Cria o backup e move o arquivo 2
+echo "Criando diretório 'backup' e movendo o arquivo3 para ele"
 mkdir backup
-
-mv arquivo3.txt /backup
+mv arquivo3.txt backup/
+echo "movido o arquivo"
 enter
 
-#Mostrar a estrutura
-ls -R meu_projeto
+#Mostrar a estrutura de pastas e arquivos.
+echo "7. Estrutura atual"
+ls -R
 enter
 
-#Apaga tudo 
+#Remover arquivos e pastas criadas 
+echo "8. Removendo arquivos e pastas..."
+rm -f arquivo1.txt notas.txt
+rm -r backup
+echo "Pastas e arquivos removidos"
 
-rm -r 
-enter
-#Voltar ao diretório anterior e apagar 
-
-cd .. rm -r meu_projeto
+#Removendo a pasta
+echo "9. Voltando ao diretorio e apagando o projeto"
+cd ..
+rm -r meu_projeto
+echo "Projeto removido"
 enter
 
 echo "=== FIM DO SCRIPT ==="
